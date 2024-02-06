7e4569d657e661e65500215c328086a95ade7e8d
Samarth Kulkarni <samarthkulkarni34@gmail.com>
2024-02-07 00:28:39 +0530

    Hello World Label

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+def window(hello):
+   app = QApplication(sys.argv)
+   w = QWidget()
+   b = QLabel(w)
+   b.setText(hello)
+   w.setGeometry(0,0,1000,500)
+   b.move(250,250)
+   w.setWindowTitle("PyQt5")
+   w.show()
+   sys.exit(app.exec_())
+   
+
+window()
